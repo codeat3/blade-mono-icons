3e42943438eebcf52c95638352864ed6f6ac1eef
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2023-02-18 14:20:23 +0000

    Fix styling

--- src/BladeMonoIconsServiceProvider.php
+++ src/BladeMonoIconsServiceProvider.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Codeat3\BladeMonoIcons;
 
 use BladeUI\Icons\Factory;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
 
 final class BladeMonoIconsServiceProvider extends ServiceProvider
 {
@@ -17,24 +17,24 @@ final class BladeMonoIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-mono-icons', []);
 
-            $factory->add('mono-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('mono-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-mono-icons.php', 'blade-mono-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-mono-icons.php', 'blade-mono-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-mono-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-mono-icons'),
             ], 'blade-mono-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-mono-icons.php' => $this->app->configPath('blade-mono-icons.php'),
+                __DIR__ . '/../config/blade-mono-icons.php' => $this->app->configPath('blade-mono-icons.php'),
             ], 'blade-mono-icons-config');
         }
     }
